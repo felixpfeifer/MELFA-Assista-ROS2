b7d5e24455207a24e4c38cb558ab736253196a9a
Felix Pfeifer <fpfeifer@stud.hs-heilbronn.de>
2024-05-08 10:58:30 +0200

    Added the Moveit Config

--- melfa_assista_moveit_config/launch/spawn_controllers.launch.py
+++ melfa_assista_moveit_config/launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("Assista", package_name="melfa_assita_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("Assista", package_name="melfa_assista_moveit_config").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
